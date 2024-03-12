0e830e331bbb6811f1d924a22dc07ed16b44300e
omar <2070omarfaruk@gmail.com>
2024-03-13 00:10:34 +0600

    created model -a

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// use App\Http\Controllers\ProductController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Route::resources(['products' => ProductController::class,]);
+// Route::apiResource('/products', 'App\Http\Controllers\ProductController',['names'=>'admin.product']);
+Route::apiResource('/products', 'App\Http\Controllers\ProductController');
+
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews', 'App\Http\Controllers\ReviewController');
+
 });
